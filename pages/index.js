52569bf40bfa77a271fabf2383bd5ede14d353cb
Xat <i@xatloon.me>
2023-10-02 15:46:55 +0800

    refactor: use `gap` instead of duplicated `my`

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen py-20 flex flex-col items-center md:max-w-3xl m-auto">
+      <main className="min-h-screen py-20 flex flex-col gap-10 items-center md:max-w-3xl m-auto">
         
         <img src="/img/opencat.png" className="w-32 h-32" />
         
@@ -16,10 +16,10 @@ export default function Home() {
           Welcome to OpenCat
         </h1>
         
-        <p className="leading-2 text-2xl my-8 text-center text-gray-500">
+        <p className="leading-2 text-2xl text-center text-gray-500">
           Download at <a className="text-blue-500 underline" href="https://apps.apple.com/app/opencat/id6445999201" target="_blank">App Store</a>
         </p>
-        <div className="mt-10 max-w-7xl mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 px-8">
+        <div className="max-w-7xl mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 px-8">
           <a href="/docs/zh-CN/help" className="hover:text-blue-500 hover:border-blue-500 flex flex-col items-start p-6 bg-white rounded-lg shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg text-gray-900">
             <h3 className="hover:text-blue-500 text-2xl font-bold mb-2">
               帮助文档
