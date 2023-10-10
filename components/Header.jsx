2078e2e32e7976ebbf187bb8e21f993255266df5
Xat <i@xatloon.me>
2023-10-11 05:16:36 +0800

    feat: add z-index and home page link to `Header`

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,16 @@
 import { IconSun, IconMoon } from "@tabler/icons-react"
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="flex justify-between px-8 py-6 w-full md:max-w-3xl mx-auto sticky top-0 items-center bg-gray-100/25 dark:bg-gray-800/75 backdrop-blur-xl " >
+    <div className="flex justify-between px-8 py-6 w-full md:max-w-3xl mx-auto sticky top-0 items-center bg-gray-100/25 dark:bg-gray-800/75 backdrop-blur-xl z-30" >
       <div className="basis-1/4">
       </div>
       <div className="mx-auto">
-        <img className="h-4 dark:hidden" src="/img/logo-light.svg" alt="logo" />
-        <img className="h-4 hidden dark:block" src="/img/logo-dark.svg" alt="logo" />
+        <Link href="/">
+          <img className="h-4 dark:hidden" src="/img/logo-light.svg" alt="logo" />
+          <img className="h-4 hidden dark:block" src="/img/logo-dark.svg" alt="logo" />
+        </Link>
       </div>
       <div id="theme-toggle" className="flex basis-1/4 justify-end">
         <IconSun className="h-6 w-6 dark:hidden" />
