fe000f5014b6cb72d89e1f8247572ebf1a1d9e97
Xat <i@xatloon.me>
2023-10-12 04:02:44 +0800

    refactor: adjust card style

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,9 +5,9 @@ export const Card = ({ title, description, link, icon }) => {
   if (typeof title === 'string' && typeof description === 'string' && typeof link === 'string') {
     return (
       <Link href={link} className="group flex p-6 bg-gray-200 dark:bg-gray-700 rounded-2xl gap-4 items-center w-full">
-        { typeof icon === 'string' && icon && <Image src={icon} alt={title} width={50} height={50} className='h-14 w-14' />}
+        { typeof icon === 'string' && icon && <Image src={icon} alt={title} width={50} height={50} className='h-12 w-12' />}
         <div className="flex flex-col items-start">
-          <div className="group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r from-blue-500 to-cyan-500 font-bold mb-2">
+          <div className="group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r from-blue-500 to-cyan-500 font-bold">
             {title}
           </div>
           <p className="text-gray-600 dark:text-gray-400">{description}</p>
@@ -19,9 +19,9 @@ export const Card = ({ title, description, link, icon }) => {
       <div className='flex flex-col items-start bg-gray-200 dark:bg-gray-700 rounded-2xl divide-y divide-gray-300 dark:divide-gray-600'>
         {title.map((t, i) => (
           <Link key={i} href={link[i]} className="group flex p-6 gap-4 items-center w-full">
-            { Array.isArray(icon) && icon[i] && <Image src={icon[i]} alt={t} width={50} height={50} className='h-14 w-14' />}
+            { Array.isArray(icon) && icon[i] && <Image src={icon[i]} alt={t} width={50} height={50} className='h-12 w-12' />}
             <div className="flex flex-col items-start">
-              <div className="group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r from-blue-500 to-cyan-500 font-bold mb-2">
+              <div className="group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r from-blue-500 to-cyan-500 font-bold">
                 {t}
               </div>
               <p className="text-gray-600 dark:text-gray-400">
