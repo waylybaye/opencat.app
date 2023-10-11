7c1910cf7102b7719150fe0848d87cfca84fd19e
Xat <i@xatloon.me>
2023-10-11 16:33:59 +0800

    refactor: adjust card style

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export const Card = ({ title, description, link }) => {
   return (
-    <Link href={ link } className="hover:text-blue-500 hover:border-blue-500 flex flex-col items-start p-6 bg-gray-200 dark:bg-gray-700 rounded-2xl">
-            <div className="hover:text-blue-500 text-2xl font-bold mb-2">
+    <Link href={ link } className="group flex flex-col items-start p-6 bg-gray-200 dark:bg-gray-700 rounded-2xl">
+            <div className="group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r from-cyan-500 to-blue-500 text-2xl font-bold mb-2">
               { title }
             </div>
             <p className="text-base">
