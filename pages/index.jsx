b0129af66915762f4424e4f848a458cbe13af0d0
Xat <i@xatloon.me>
2023-10-12 11:58:42 +0800

    fix: add app-argument to meta tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div>
       <Head>
         <meta name="description" content="OpenCat is a native AI chat client, offering a smoother and faster chat experience." />
-        <meta name="apple-itunes-app" content="app-id=6445999201" />
+        <meta name="apple-itunes-app" content="app-id=6445999201 app-argument=https://opencat.app/" />
         <title>OpenCat - Native iOS/macOS/iPadOS client for OpenAI/ChatGPT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
