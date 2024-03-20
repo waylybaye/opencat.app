fd8c75f5eec599cd2076878fb494dc431b7444d3
Xat <i@xatloon.com>
2024-03-20 13:33:44 +0800

    fix: adjust release download link

--- src/app/releases/route.ts
+++ src/app/releases/route.ts
@@ -1,27 +1,5 @@
-import { readFile } from 'node:fs/promises'
-import path from 'node:path'
-import process from 'node:process'
-import { XMLParser } from 'fast-xml-parser'
-import type { NextRequest } from 'next/server'
+import { redirect } from 'next/navigation'
 
-export async function GET(request: NextRequest) {
-  const reqVersion = request.url.split('/').pop() || 'latest'
-  const castPath = path.join(process.cwd(), 'content/releases', 'versions.xml')
-  const xml = await readFile(castPath, 'utf8')
-  const parser = new XMLParser()
-  const cast = parser.parse(xml)
-  const latest = `${cast.rss.channel.item[0]['sparkle:shortVersionString']}.${cast.rss.channel.item[0]['sparkle:version']}`
-  const version = reqVersion === 'latest' ? latest : reqVersion.replace('OpenCat-', '').replace('.dmg', '')
-  const buffer = await readFile(path.join(process.cwd(), 'content/releases', `OpenCat-${version}.dmg`))
-  return new Response(buffer, {
-    headers: {
-      'Content-Type': 'application/octet-stream',
-      'Content-Disposition': `attachment; filename="OpenCat-${version}.dmg"`,
-    },
-  })
-  return new Response(xml, {
-    headers: {
-      'Content-Type': 'application/xml',
-    },
-  })
+export async function GET() {
+  redirect('/releases/latest')
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -26,7 +26,7 @@ export function middleware(request: NextRequest) {
   if (
     [
       'img',
-      'release',
+      'releases',
       'docs',
       // Add other folders/files in `public` that you want to ignore
     ].includes(pathname.split('/')[1])
