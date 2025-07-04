748fd64ea4c8004e9a72ba2fb3cad1d32a012687
Baye <596620+waylybaye@users.noreply.github.com>
2025-07-05 00:24:42 +0700

    fix: /release redirect to dmg file

--- src/app/release/route.ts
+++ src/app/release/route.ts
@@ -9,11 +9,12 @@ export async function GET() {
   const parser = new XMLParser()
   const cast = parser.parse(xml)
   const latest = `${cast.rss.channel.item[0]['sparkle:shortVersionString']}.${cast.rss.channel.item[0]['sparkle:version']}`
-  const buffer = await readFile(path.join(process.cwd(), 'public/releases', `OpenCat-${latest}.dmg`))
-  return new Response(buffer, {
+  
+  // Redirect to the static file instead of reading it into memory
+  return new Response(null, {
+    status: 302,
     headers: {
-      'Content-Type': 'application/octet-stream',
-      'Content-Disposition': `attachment; filename="OpenCat-${latest}.dmg"`,
+      'Location': `/releases/OpenCat-${latest}.dmg`,
     },
   })
 }
